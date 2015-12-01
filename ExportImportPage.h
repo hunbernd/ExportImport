837fc805d3e2f57cb8898a3d955ffbae3d337fc7
Philip Wales <ptwales@gmail.com>
2015-11-30 18:28:03 -0600

    removed useless doxygen section

--- ExportImportPage.h
+++ ExportImportPage.h
@@ -5,28 +5,7 @@
 #include <retroshare/rsplugin.h>
 #include <retroshare/rsiface.h>
 #include "ui_ExportImportPage.h"
-/*!
- * \file
- * \author Anonymous
- *
- * \section LICENSE
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
- * at https://www.gnu.org/copyleft/gpl.html
- *
- * \section DESCRIPTION
- *
- * \brief The ExportImportPage defines the UI behavior in Retroshare.
- *
- * The ExportImport UI has two methods of importing and exporting peers: reading
- * and writing to json files, or copy and pasting json text in a text box.
- */
+
 class ExportImportPage : public MainPage
 {
     Q_OBJECT
